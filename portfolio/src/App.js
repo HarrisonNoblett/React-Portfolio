f1af59781bd96464b2ee0cd179d9495dcbe9e99d
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-03-21 18:26:11 -0500

    working MVP of application

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,20 @@
-import Footer from './pages/Home';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-   <Home/>
+    <Router>
+    <div className="App">
+       
+      <Switch>
+        
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/Home" component={Home} /> */}
+        
+      </Switch>
+
+    </div>
+    </Router>
   );
 }
 
--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,3 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import Card from "../components/Card";
 import Footer from "../components/Footer";
+
+function Home() {
+    return (
+        <div className="container">
+            <Header />
+            <Card />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
